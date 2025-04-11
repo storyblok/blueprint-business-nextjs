b88f329ba4a4da7b876f345daa881db9fa37b744
Silas Getachew Merdasa <silogecho@gmail.com>
2025-04-11 10:31:03 +0300

    fix: update  endpoint URL

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,8 @@ storyblokInit({
   use: [apiPlugin],
   components,
   apiOptions: {
-    endpoint: process.env.STORYBLOK_API_BASE_URL ? `${new URL('process.env.STORYBLOK_API_BASE_URL').origin}/v2` : undefined,
+    endpoint: process.env.STORYBLOK_API_BASE_URL ? `${new URL(process.env.STORYBLOK_API_BASE_URL).origin}/v2` : undefined,
+
   }
 });
 
